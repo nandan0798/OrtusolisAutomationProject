685846b780e74f4bc9946c379d40e77092bd4962
nandan0798 <nandannandhu3@gmail.com>
2025-05-19 12:17:56 +0530

    Original code before implementing xlsx files

---
+++ src/main/java/ExcelUtils/ExcelUtils.java
@@ -0,0 +1,42 @@
+package ExcelUtils;
+
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.*;
+
+public class ExcelUtils {
+    private String filePath;
+    private Workbook workbook;
+    private Sheet sheet;
+
+    public ExcelUtils(String filePath, String sheetName) throws IOException {
+        this.filePath = filePath;
+        FileInputStream fis = new FileInputStream(filePath);
+        workbook = new XSSFWorkbook(fis);
+        sheet = workbook.getSheet(sheetName);
+    }
+
+    public String getCellData(int rowNum, int colNum) {
+        Cell cell = sheet.getRow(rowNum).getCell(colNum);
+        return cell.toString();
+    }
+
+    public void setCellData(int rowNum, int colNum, String value) throws IOException {
+        Row row = sheet.getRow(rowNum);
+        if (row == null) row = sheet.createRow(rowNum);
+
+        Cell cell = row.getCell(colNum);
+        if (cell == null) cell = row.createCell(colNum);
+
+        cell.setCellValue(value);
+
+        FileOutputStream fos = new FileOutputStream(filePath);
+        workbook.write(fos);
+        fos.close();
+    }
+
+    public void closeWorkbook() throws IOException {
+        workbook.close();
+    }
+}
